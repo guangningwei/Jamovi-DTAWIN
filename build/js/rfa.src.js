b34e272f1c992b1d21e6b6a5f59dd94c12d7130c
guangningwei <wei.guangning1997@gmail.com>
2021-04-06 20:02:13 -0400

    upload

---
+++ build/js/rfa.src.js
@@ -0,0 +1,93 @@
+
+// This file is an automatically generated and should not be edited
+
+'use strict';
+
+const options = [{"name":"data","type":"Data"},{"name":"dep","title":"Dependent Variable","type":"Variable"},{"name":"indep","title":"Independent Variable/s","type":"Variables"},{"name":"split","title":"Split train/test data (60-90)","type":"Number","min":60,"max":90,"default":75},{"name":"ntree","title":"Number of Tree","type":"Number","min":100,"max":1000,"default":500}];
+
+const view = function() {
+    
+    this.handlers = { }
+
+    View.extend({
+        jus: "3.0",
+
+        events: [
+
+	]
+
+    }).call(this);
+}
+
+view.layout = ui.extend({
+
+    label: "Random Forest Analysis",
+    jus: "3.0",
+    type: "root",
+    stage: 0, //0 - release, 1 - development, 2 - proposed
+    controls: [
+		{
+			type: DefaultControls.VariableSupplier,
+			typeName: 'VariableSupplier',
+			persistentItems: false,
+			stretchFactor: 1,
+			controls: [
+				{
+					type: DefaultControls.TargetLayoutBox,
+					typeName: 'TargetLayoutBox',
+					label: "Dependent Variable",
+					controls: [
+						{
+							type: DefaultControls.VariablesListBox,
+							typeName: 'VariablesListBox',
+							name: "dep",
+							maxItemCount: 1,
+							isTarget: true
+						}
+					]
+				},
+				{
+					type: DefaultControls.TargetLayoutBox,
+					typeName: 'TargetLayoutBox',
+					label: "Independent Variable/s",
+					controls: [
+						{
+							type: DefaultControls.VariablesListBox,
+							typeName: 'VariablesListBox',
+							name: "indep",
+							isTarget: true
+						}
+					]
+				}
+			]
+		},
+		{
+			type: DefaultControls.LayoutBox,
+			typeName: 'LayoutBox',
+			margin: "large",
+			controls: [
+				{
+					type: DefaultControls.TextBox,
+					typeName: 'TextBox',
+					name: "split",
+					format: FormatDef.number
+				}
+			]
+		},
+		{
+			type: DefaultControls.LayoutBox,
+			typeName: 'LayoutBox',
+			margin: "large",
+			controls: [
+				{
+					type: DefaultControls.TextBox,
+					typeName: 'TextBox',
+					name: "ntree",
+					format: FormatDef.number
+				}
+			]
+		}
+	]
+});
+
+module.exports = { view : view, options: options };
